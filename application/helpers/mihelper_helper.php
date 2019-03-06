1385358276c264c7225acf11be120c7f6e374e2e
Eduardo Rondon <erondont@gmail.com>
2019-03-06 18:16:30 -0500

    Proyecto Vacio

---
+++ application/helpers/mihelper_helper.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+if ( ! function_exists(''))
+{
+    function maxId($tabla = '')
+    {
+        $ci =& get_instance();
+        $sql = "select max(id) as idx from ".$tabla;
+        $query = $ci->db->query($sql);
+        $row = $query->row();
+        if(isset($row)){
+            return $row->idx;
+        }
+    }
+}
+*/
